01c6ecb7e2d509584dd34ae00675b023f6ac097b
Stas Deruchenko <stas.dko@yandex.ru>
2025-09-27 16:00:02 +0300

    Remove solution folder and its contents

--- solution/urls.py
+++
@@ -1,6 +0,0 @@
-class URLS:
-    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/'
-    AUTH_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login'
-    REG_PAGE_URL = 'https://stellarburgers.nomoreparties.site/register'
-    RECOVER_PAGE_URL = 'https://stellarburgers.nomoreparties.site/forgot-password'
-    PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile'
