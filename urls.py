1a7adfe0bd271b370f604f8a249d617c0f6e4874
Stas Deruchenko <stas.dko@yandex.ru>
2025-10-02 22:05:13 +0300

    Add Sprint_5 solution

---
+++ urls.py
@@ -0,0 +1,6 @@
+class URLS:
+    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/'
+    AUTH_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login'
+    REG_PAGE_URL = 'https://stellarburgers.nomoreparties.site/register'
+    RECOVER_PAGE_URL = 'https://stellarburgers.nomoreparties.site/forgot-password'
+    PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile'
